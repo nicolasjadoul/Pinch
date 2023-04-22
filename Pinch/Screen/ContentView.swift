8507fad1e24590f4fb4a788e6bfd029b84f6724f
nicolasjadoul <jadoul.nico@yahoo.fr>
2023-04-22 12:44:01 +0200

    upd view

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var isAnimating: Bool = false
+    @State private var imageScaling: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScaling = 1
+            imageOffset = .zero
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+//                    .scaledToFit()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScaling)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScaling == 1 {
+                            withAnimation(.spring()) {
+                                imageScaling = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    })
+                    .gesture(DragGesture()
+                        .onChanged { value in
+//                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+//                            }
+                        }
+                        .onEnded {_ in
+                            if imageScaling <= 1 {
+                                resetImageState()
+                            }
+                        })
+            }
+            .navigationTitle("Pinch & Zool")
+            .foregroundColor(.black)
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration : 1)) {
+                    isAnimating = true
+                }
+            })
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
