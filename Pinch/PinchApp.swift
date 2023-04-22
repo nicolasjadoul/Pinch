2b442ebfdea408eb3dc90f7406bc6c5900737a31
nicolasjadoul <jadoul.nico@yahoo.fr>
2023-04-22 11:39:58 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jadoul Nicolas on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
