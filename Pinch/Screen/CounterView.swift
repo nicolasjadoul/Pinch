95fa92b20e48a69d1cee0c83dec45ee90328e2cd
nicolasjadoul <jadoul.nico@yahoo.fr>
2023-04-23 13:28:02 +0200

    final commit

--- Pinch/Screen/CounterView.swift
+++ Pinch/Screen/CounterView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct CounterView: View {
+    @State var counter: String = "hello"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button() {
+            counter = counter + " world"
+        } label: {
+            Text(counter)
+//            Text($counter)
+        }
+    }
+}
+
+struct ContentView2: View {
+    @State var greeting: Int = 0
+    var body: some View {
+        SwiftUIViewA(greeting: greeting)
+            .onTapGesture {
+                greeting += 1
+            }
+    }
+}
+
+struct SwiftUIViewA: View {
+    var greeting:Int
+    var body: some View {
+        Text("\(greeting)")
     }
 }
 
